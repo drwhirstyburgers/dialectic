232e994e780479fde377805fecf0a8c365dad009
Drwhirstyburgers <dave.hirst50@gmail.com>
2019-05-18 17:58:56 -0400

    notes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+//run foreman = foreman start -f Procfile.dev
+
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
